4c0267a2daceadf592f71a00374a18084b32b969
M Steph <mbigha.k.steph@gmail.com>
2025-01-16 14:21:22 +0100

    passive scanning scripts added

--- src/main/java/org/example/PassiveScanning.java
+++ src/main/java/org/example/PassiveScanning.java
@@ -1,14 +1,14 @@
-//package org.example;
-//
-//import org.zaproxy.clientapi.core.ClientApi;
-//import org.zaproxy.clientapi.core.ClientApiException;
-//import org.zaproxy.clientapi.gen.Pscan;
-//
-//public class PassiveScanning {
-//    static void startPassiveScan(ClientApi api) throws ClientApiException {
-//        System.out.println("Starting passive scan...");
-//        Pscan pscan = new Pscan(api);
-//        pscan.setEnabled("true");
-//        System.out.println("Passive scan enabled.");
-//    }
-//}
+package org.example;
+
+import org.zaproxy.clientapi.core.ClientApi;
+import org.zaproxy.clientapi.core.ClientApiException;
+import org.zaproxy.clientapi.gen.Pscan;
+
+public class PassiveScanning {
+   static void startPassiveScan(ClientApi api) throws ClientApiException {
+      System.out.println("Starting passive scan...");
+        Pscan pscan = new Pscan(api);
+       pscan.setEnabled("true");
+     System.out.println("Passive scan enabled.");
+  }
+}
